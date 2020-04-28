4c7c41237b84477b4f99ded2e209da5d07c964c5
立花冠 <tachibanakan@tachibanakannoMacBook-Pro.local>
2020-04-28 18:08:42 +0900

    progress

---
+++ src/AtcoderBeginnersSelection/ABC087B_Coins/Main.java
@@ -0,0 +1,94 @@
+package AtcoderBeginnersSelection.ABC087B_Coins;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        FastScanner sc = new FastScanner();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int x = sc.nextInt();
+        int count = 0;
+
+        for (int i = 0; i <= a; i++){
+            for(int j = 0; j <= b; j++){
+                for (int k = 0; k <= c; k++){
+                    if (i * 500 + j * 100 + k * 50 == x) count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+
+    }
+
+
+    static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1024];
+        private int ptr = 0;
+        private int buflen = 0;
+        private boolean hasNextByte() {
+            if (ptr < buflen) {
+                return true;
+            }else{
+                ptr = 0;
+                try {
+                    buflen = in.read(buffer);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (buflen <= 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private int readByte() { if (hasNextByte()) return buffer[ptr++]; else return -1;}
+        private static boolean isPrintableChar(int c) { return 33 <= c && c <= 126;}
+        public boolean hasNext() { while(hasNextByte() && !isPrintableChar(buffer[ptr])) ptr++; return hasNextByte();}
+        public String next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            StringBuilder sb = new StringBuilder();
+            int b = readByte();
+            while(isPrintableChar(b)) {
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+        public long nextLong() {
+            if (!hasNext()) throw new NoSuchElementException();
+            long n = 0;
+            boolean minus = false;
+            int b = readByte();
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+            if (b < '0' || '9' < b) {
+                throw new NumberFormatException();
+            }
+            while(true){
+                if ('0' <= b && b <= '9') {
+                    n *= 10;
+                    n += b - '0';
+                }else if(b == -1 || !isPrintableChar(b)){
+                    return minus ? -n : n;
+                }else{
+                    throw new NumberFormatException();
+                }
+                b = readByte();
+            }
+        }
+        public int nextInt() {
+            long nl = nextLong();
+            if (nl < Integer.MIN_VALUE || nl > Integer.MAX_VALUE) throw new NumberFormatException();
+            return (int) nl;
+        }
+        public double nextDouble() { return Double.parseDouble(next());}
+    }
+}
---
+++ src/AtcoderBeginnersSelection/Product.java
@@ -0,0 +1,15 @@
+package AtcoderBeginnersSelection;
+
+import java.util.*;
+
+public class Product {
+    public static void main(String[] args){
+     Scanner sc = new Scanner(System.in);
+     int a = sc.nextInt();
+     int b = sc.nextInt();
+     int result = a * b;
+     if(result % 2 == 0){
+         System.out.println("Even");
+     }else System.out.println("Odd");
+    }
+}
